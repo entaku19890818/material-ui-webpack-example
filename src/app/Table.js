d1edc191f1c046f2fb685892e4b81fb04a0b82a5
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2017-04-24 18:20:13 +0900

    画像追加してlukeをついかした

--- src/app/Table.js
+++ src/app/Table.js
@@ -10,8 +10,9 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import IconButton from 'material-ui/IconButton';
 import AddCircle from 'material-ui/svg-icons/content/add-circle';
-
-
+import FlatButton from 'material-ui/FlatButton';
+import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
 const styles = {
 	largeIcon: {
 		width: 60,
@@ -33,20 +34,32 @@ const muiTheme = getMuiTheme({
 
 class Table extends Component {
 
+	state = {
+    open: false,
+  };
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  }
+	handleOpen = () => {
+    this.setState({open: true});
+  };
 
-  handleTouchTap = () => {
-    this.setState({
-      open: true,
-    });
-  }
+  handleClose = () => {
+    this.setState({open: false});
+  };
 
   render() {
+		const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
@@ -54,12 +67,27 @@ class Table extends Component {
 				<IconButton
 					iconStyle={styles.largeIcon}
 					style={styles.large}
-					tooltip="ディエゴ"
+					tooltip="luke"
 					touch={true}
 					tooltipPosition="top-left"
+					onTouchTap={this.handleOpen}
 					>
 					<AddCircle />
 				</IconButton>
+				<Dialog
+					title="Dialog With Actions"
+					actions={actions}
+					modal={false}
+					open={this.state.open}
+					onRequestClose={this.handleClose}
+				>
+					The actions in this window were passed in as an array of React objects.
+					<form onSubmit={this.handleSubmit}>
+						<DatePicker hintText="Landscape Inline Dialog" container="inline" mode="landscape" autoOk={true}/>
+						<TimePicker	hintText="startTime" autoOk={true}/>
+						<TimePicker	hintText="endTime" autoOk={true}/>
+					</form>
+				</Dialog>
         </div>
       </MuiThemeProvider>
     );
